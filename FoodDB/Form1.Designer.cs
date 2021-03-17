f46eaad8fa226792a7b3577ad3256d73073fa389
Twyxter <johannes.kiigemagi@gmail.com>
2021-03-17 21:47:24 +0200

    Food DB

---
+++ FoodDB/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FoodDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listFood = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listFoodNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Food";
+            // 
+            // listFood
+            // 
+            this.listFood.FormattingEnabled = true;
+            this.listFood.Location = new System.Drawing.Point(157, 150);
+            this.listFood.Name = "listFood";
+            this.listFood.Size = new System.Drawing.Size(209, 95);
+            this.listFood.TabIndex = 1;
+            this.listFood.SelectedIndexChanged += new System.EventHandler(this.listFood_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Food Names";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // listFoodNames
+            // 
+            this.listFoodNames.FormattingEnabled = true;
+            this.listFoodNames.Location = new System.Drawing.Point(422, 150);
+            this.listFoodNames.Name = "listFoodNames";
+            this.listFoodNames.Size = new System.Drawing.Size(232, 95);
+            this.listFoodNames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listFoodNames);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listFood);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listFood;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listFoodNames;
+    }
+}
+
---
+++ FoodDB/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodDB
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["FoodDB.Properties.Settings.FoodConnectionString"].ConnectionString;
+        }
+        private void PopulateFoodTable()
+        {
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM FoodType", connection))
+            {
+                DataTable foodTable = new DataTable();
+                adapter.Fill(foodTable);
+
+                listFood.DisplayMember = "TypeName";
+                listFood.ValueMember = "Id";
+                listFood.DataSource = foodTable;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateFoodTable();
+        }
+
+        private void PopulateFoodNames()
+        {
+            string query = "SELECT Food.Name FROM FoodType INNER JOIN FoodInTheFridge ON FoodInTheFridge.TypeId=FoodType.Id WHERE FoodType.Id=@TypeId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@TypeId", listFood.SelectedValue);
+                DataTable foodNameTable = new DataTable();
+                adapter.Fill(foodNameTable);
+
+                listFoodNames.DisplayMember = "Name";
+                listFoodNames.ValueMember = "Id";
+                listFoodNames.DataSource = foodNameTable;
+            }
+
+        }
+
+        private void listFood_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateFoodNames();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
